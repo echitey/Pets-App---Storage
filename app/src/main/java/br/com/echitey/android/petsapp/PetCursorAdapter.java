6efc02aa61bf7914b4dc839041986d59b927caec
Yawo Echitey <yawo@echitey.com>
2020-05-10 00:21:06 -0400

    Added Unknown Breed Showing

--- app/src/main/java/br/com/echitey/android/petsapp/PetCursorAdapter.java
+++ app/src/main/java/br/com/echitey/android/petsapp/PetCursorAdapter.java
@@ -2,6 +2,7 @@ package br.com.echitey.android.petsapp;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -60,6 +61,12 @@ public class PetCursorAdapter extends CursorAdapter {
         // Read the pet attributes from the Cursor for the current pet
         String petName = cursor.getString(nameColumnIndex);
         String petBreed = cursor.getString(breedColumnIndex);
+
+        // If the pet breed is empty string or null, then use some default text
+        // that says "Unknown breed", so the TextView isn't blank.
+        if (TextUtils.isEmpty(petBreed)) {
+            petBreed = "Unknown breed";
+        }
 
         // Update the TextViews with the attributes for the current pet
         nameTextView.setText(petName);
